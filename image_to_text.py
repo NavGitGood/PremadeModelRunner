209999351b62ea5e6ac65ba90379a0bd6e79680a
N G <nnav4u@gmail.com>
2024-03-20 15:35:24 +0100

    updated instructions

--- image_to_text.py
+++ image_to_text.py
@@ -2,7 +2,9 @@ from diffusers import StableDiffusionPipeline
 import torch
 
 model_id = "runwayml/stable-diffusion-v1-5"
+#change to float16 if on Nvidia gpu
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
+#uncomment if on Nvidia gpu
 #pipe = pipe.to("cuda")
 
 prompt = "a photo of a hippo riding a monkey on saturn's rings"
